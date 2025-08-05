03d88e5923cb430852f64be3acd3c92f248fe1bd
Bill Harrison <william.lawrence.harrison@gmail.com>
2025-08-05 15:03:00 -0400

    fast forward

---
+++ src/aes/Basic.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+module AES.Basic(State , Column , RoundKey) where
+
+import ReWire
+import ReWire.Vectors
+
+type State    = Vec 4 (Vec 4 (W 8))
+type Column   = Vec 4 (W 8)
+type RoundKey = Vec 4 (Vec 4 (W 8))
+
+-- type SBox   = Vec 0x10 (Vec 0x10 (W 8))
+-- type Index  = Finite 0x10
+
